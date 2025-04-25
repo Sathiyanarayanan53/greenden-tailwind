8eb32fd37d1e7c544388e86edf47443b7e3d570b
sathiyanarayanan53 <sathyanarayanan5303@gmail.com>
2025-04-25 13:27:35 +0530

    updated successfully

---
+++ contact.js
@@ -0,0 +1,10 @@
+var closenav=document.getElementById("closenav")
+var sidenav=document.getElementById("sidenav")
+var menuicon=document.getElementById("menuicon")
+
+menuicon.addEventListener("click",function(){
+    sidenav.style.right=0
+})
+closenav.addEventListener("click",function(){
+    sidenav.style.right="-50%" 
+})
