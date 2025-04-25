987de53a9be7704095be98a95498a494a6f733aa
sathiyanarayanan53 <sathyanarayanan5303@gmail.com>
2025-04-25 20:05:46 +0530

    updated successfully

--- product.js
+++ product.js
@@ -8,3 +8,24 @@ menuicon.addEventListener("click",function(){
 closenav.addEventListener("click",function(){
     sidenav.style.right="-50%" 
 })
+
+var search=document.getElementById("search")
+var productcontainer=document.getElementById("productcontainer")
+var productlist=productcontainer.querySelectorAll("div")
+
+
+search.addEventListener("keyup",function(){
+    var enteredvalue= event.target.value.toUpperCase()
+
+    for(count=0;count<productlist.length;count++)
+    {
+        var productitem=productlist[count].querySelector("h1").textContent
+
+        if(productitem.toUpperCase().indexOf(enteredvalue)<0){
+            productlist[count].style.display="none"
+        }
+        else{
+            productlist[count].style.display="block"
+        }
+    }
+})
